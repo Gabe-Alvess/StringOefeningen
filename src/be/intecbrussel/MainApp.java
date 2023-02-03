e6d7d0e1ae8ad96258507219909f0eb4eaf91317
Biel-Alves <gabriel_vieira2018@hotmail.com>
2023-02-03 17:45:42 +0100

    update

--- src/be/intecbrussel/MainApp.java
+++ src/be/intecbrussel/MainApp.java
@@ -4,7 +4,7 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("Github");
         System.out.println(" ---- Oefening 1 ---- ");
-        // Ken volgende waarde aan een string toe Java Exercises!,
+        // 1. Ken volgende waarde aan een string toe Java Exercises!,
         // gebruik nu een functie van de String class en druk
         // enkel het volgende woord Exercises af van de variabele.
 
@@ -12,14 +12,14 @@ public class MainApp {
         System.out.println(les.substring(4, 14));
 
         System.out.println(" ---- Oefening 2 ---- ");
-        // Maak een string met volgende waarde walter, edwin, mike
+        // 2. Maak een string met volgende waarde walter, edwin, mike
         // controlleer of de naam edwin voorkomt in de string.
 
         String ListOfNames = "walter, edwin, mike";
         System.out.println(ListOfNames.contains("edwin"));
 
         System.out.println("---- Oefening 3 ---- ");
-        // Maak een string met de volgende waarde hello world maak nu een algoritme dat van de huidige waarde het
+        // 3. Maak een string met de volgende waarde hello world maak nu een algoritme dat van de huidige waarde het
         // volgende maakt HeLlO WoRlD. Bekijk voor deze oefening zeker de documentatie
         // van Class String van oracle en de vorige hoofdstukken.
 
@@ -39,10 +39,49 @@ public class MainApp {
         System.out.println("---- String - Oefenreeks 1 ----");
 
         System.out.println("---- Oefening - 1 ----");
-        // Maak een string aan en print deze naar de console.
+        // 1. Maak een string aan en print deze naar de console.
+
         String str = "Hello";
         System.out.println(str);
 
-        System.out.println("---- Oefenig - 2 ----");
+        System.out.println("---- Oefening - 2 ----");
+        // 2. Gebruik de ".length()" eigenschap om de lengte van een string of stringBuilder te bepalen.
+
+        String str1 = "Hello World";
+        System.out.println(str1.length());
+
+        System.out.println("---- Oefening - 3 ----");
+        // 3. Gebruik de .substring() methode om een deel van een string of stringBuilder
+        // te selecteren en print dit naar de console.
+
+        String str2 = "Hello World";
+        System.out.println(str2.substring(6, 11));
+
+        System.out.println("---- Oefening - 4 ----");
+        // 4. Gebruik de + operator of de concat() methode om twee strings samen te voegen.
+
+        String str3 = "Hello ";
+        String str4 = "There!";
+        String strConcat = str3.concat(str4);
+        System.out.println(strConcat + " -> met .concat()");
+        System.out.println(str3 + str4 + " -> met + operator");
+
+        System.out.println("---- Oefening - 5 ----");
+        // 5. Maak een programma dat controleert of een String een geldige email address is.
+        // (email address moet minstens een @ en een . bevatten, de . moet na de @ komen).
+
+        String email = "@.";
+
+        boolean containsAt = email.contains("@");
+        boolean containsDot = email.contains(".");
+
+        int atPosition = email.indexOf("@");
+        int dotPosition = email.indexOf(".");
+
+        if (containsAt && containsDot && atPosition < dotPosition) {
+            System.out.println(email + " ->" + " Is een geldige email address!");
+        } else {
+            System.out.println(email + " ->" + " Is een ongeldige email address!");
+        }
     }
 }
